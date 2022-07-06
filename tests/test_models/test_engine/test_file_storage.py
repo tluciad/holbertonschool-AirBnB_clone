e4249b3f29e29d3a563f08c5aa98d5a7414ddb37
Edwin-9603 <egc4611@gmail.com>
2022-07-06 22:32:11 +0000

    new tests airbnb

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -15,42 +15,54 @@ from datetime import datetime
 
 class TestFileStorageClass(unittest.TestCase):
 
-    def test_instance_creation(self):
-        """"Test that instance of filestorage is properly created"""
-        Storage = FileStorage()
-        self.assertTrue(type(Storage) == FileStorage)
-        self.assertTrue(isinstance(Storage, FileStorage))
+	def test_instance_creation(self):
+		""""Test that instance of filestorage is properly created"""
+		Storage = FileStorage()
+		self.assertTrue(type(Storage) == FileStorage)
+		self.assertTrue(isinstance(Storage, FileStorage))
 
-    def test_storage_all(self):
-        """Tests method all"""
-        FS = FileStorage()
-        self.assertEqual(type(FS.all()), dict)
+	def test_storage_all(self):
+		"""Tests method all"""
+		FS = FileStorage()
+		self.assertEqual(type(FS.all()), dict)
 
-    def test_storage_new(self):
-        """Test method new"""
-        FS = FileStorage()
-        FS.new(BaseModel())
-        self.assertTrue(FS.all())
+	def test_storage_new(self):
+		"""Test method new"""
+		FS = FileStorage()
+		FS.new(BaseModel())
+		self.assertTrue(FS.all())
 
-    def test_storage_save(self):
-        """Test method save"""
-        FS = FileStorage()
-        B_M = BaseModel()
-        FS.new(B_M)
-        FS.save()
-        with open("file.json", "r", encoding='utf-8') as r:
-            Json_full = r.read()
-            self.assertTrue(f"BaseModel.{B_M.id}" in Json_full)
+	def test_storage_save(self):
+		"""Test method save"""
+		FS = FileStorage()
+		B_M = BaseModel()
+		FS.new(B_M)
+		FS.save()
+		with open("file.json", "r", encoding='utf-8') as r:
+			Json_full = r.read()
+			self.assertTrue(f"BaseModel.{B_M.id}" in Json_full)
 
-    def test_storage_reload(self):
-        """Test method reload"""
-        FS = FileStorage()
-        B_M = BaseModel()
-        Obj_Dict = FS.all()
-        B_M.save()
-        FS.reload()
-        Obj_Dict_save = FS.all()
-        self.assertTrue(Obj_Dict == Obj_Dict_save)
+	def test_storage_reload(self):
+		"""Test method reload"""
+		FS = FileStorage()
+		B_M = BaseModel()
+		Obj_Dict = FS.all()
+		B_M.save()
+		FS.reload()
+		Obj_Dict_save = FS.all()
+		self.assertTrue(Obj_Dict == Obj_Dict_save)
+
+	def test_reload(self):
+		"""
+		Method that tests:
+			if file storage was documented
+			and checks reload() method exists
+		"""
+		self.assertTrue(FileStorage.reload.__doc__)
+		self.assertIsNotNone(FileStorage.reload.__doc__)
+
+		m_test = FileStorage()
+		self.assertTrue(hasattr(m_test, "reload"), True)
 
 
 if __name__ == "__main__":
